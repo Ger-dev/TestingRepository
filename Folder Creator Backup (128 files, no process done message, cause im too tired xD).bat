31c1383a838a387fadd58402b06372861790393c
Ger-dev <81104410+Ger-dev@users.noreply.github.com>
2021-03-21 20:20:36 +0300

    Add files via upload

---
+++ Folder Creator Backup (128 files, no process done message, cause im too tired xD).bat
@@ -0,0 +1,7 @@
+echo off
+cd Dunno.bat test folder
+md pog
+cd pog
+md Infinite folders
+md A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% A%random% 
+start a.vbs
---
+++ README.txt
@@ -0,0 +1,17 @@
+Q: How to find the folders created?
+A: They're in the bat test folder!
+
+Q: Why is it dunno.bat?
+A: it was created as a testing program!
+
+Q: Who created this?
+A: Ger3tto
+
+Q: Will it destroy my computer?
+A: I bet not, the folders have a few kilobytes in the hard-drive, i do not recommend for small storages...
+
+Q: How to see the created folders?
+A: First, Make a folder called "Dunno.bat test folder", Then, Run the files, And, Go inside the created folder, then in the pog folder, and you have the folders created!
+
+Q: Can I modify the code?
+A: Sure! Just make sure not to make the creation infinite, as that would be unstoppable...
